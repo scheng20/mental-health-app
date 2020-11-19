0b8dc603e2fa02d6c1bfda6f02e63e9f75b38afa
Yiyi Liu <t8o1b@ugrad.cs.ubc.ca>
2020-11-19 01:15:01 -0800

    modified write-review

--- write-reviews.php
+++ write-reviews.php
@@ -1,13 +1,29 @@
 <?php
+	
+	include 'connect.php';
+	$conn = OpenCon();
+	session_start();
 
-    include 'connect.php';
-    session_start();
-    $conn = OpenCon();
+	// Check if the review is submitted
+	if(isset($_POST['ReviewSubmit'])){ 
+		processReview();
+	} 
 
-	function writeReview() {
+	function displayContents() {
+        if (processReview()) {
+            echo '<div class="alert alert-success" role="alert">
+                Review posted. Thanks for your review! </div>';
+        } else {
+            echo '<div class="alert alert-danger" role="alert">
+                Unable to post your review. Please try again.
+            </div>';
+        }
+    }
+
+	// Process the review update
+	function processReview() {
 		global $conn;
-        $id = $_SESSION["userID"];
-        $reviewAuthor = $_POST["userID"];
+        $reviewAuthor = $_SESSION["userID"];
         $counsellor = $_POST["userID"];
         $rating = $_POST["rating"];
         $feedback = $_POST["feedback"];
@@ -18,31 +34,135 @@
         return $result;
 	}
 
-	function displayContents() {
-        if (writeReview()) {
-            echo '<div class="alert alert-success" role="alert">
-                Review posted. Thanks for your review! </div>';
-        } else {
-            echo '<div class="alert alert-danger" role="alert">
-                Unable to post your review. Please try again.
-            </div>';
-        }
-    }
+	// Display the review form
+	function showReviewForm() {
+
+		global $conn; 
+		$sql = "SELECT * FROM Users WHERE userID =". $_SESSION['userID'];
+		$result = $conn->query($sql);
+
+		while($row = $result->fetch_assoc()) {
+            $sql = "SELECT name FROM Users WHERE userID=".$row["userID"];
+            $name = $conn->query($sql)->fetch_assoc()['name'];
+			echo "<option value=".$row["userID"].">$name</option>";
+		}
+	}
 	
 ?>
 
+
 <!DOCTYPE html>
 <html>
-<head>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <link rel="stylesheet" href="styles/main.css">
-</head>
+	
+	<head>
+		<!-- Bootstrap Stylesheet -->
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+		<!-- Custom Stylesheet -->
+		<link rel="stylesheet" href="styles/main.css">
+
+		<!-- Bootstrap JS -->
+		<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+	</head>
 
 	<body>
+        <!-- Navbar -->
+        <nav class="navbar navbar-expand-lg navbar-dark">
+			<a class="navbar-brand" href="#">Mental Health Webapp</a>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+		 	<div class="collapse navbar-collapse" id="navbarNav">
+		 		<ul class="navbar-nav">
+
+		 			<li class="nav-item">
+			        	<a class="nav-link" href="/cpsc304/profile.php">Profile</a>
+			      	</li>
+
+			      	<li class="nav-item dropdown">
+			        	<a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+			          	Appointments
+			        	</a>
+				        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+				        	<a class="dropdown-item" href="/cpsc304/view-appointments.php">View Appointments</a>
+				        	<a class="dropdown-item active" href="/cpsc304/book-appointments.php">Book an Appointment</a>
+				        </div>
+			      	</li>
+
+			     	<li class="nav-item dropdown">
+			        	<a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+			          		Reviews
+			        	</a>
+			        	<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+			          		<a class="dropdown-item" href="/cpsc304/view-reviews.php">View Reviews</a>
+			          		<a class="dropdown-item" href="#">Write a Review</a>
+			        	</div>
+			      	</li>
+
+			     	<li class="nav-item dropdown" aria-labelledby="navbarDropdownMenuLink">
+			        	<a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+			          		Posts
+			        	</a>
+				        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+				        	<a class="dropdown-item" href="#">View Posts</a>
+				        	<a class="dropdown-item" href="#">Write a Post</a>
+				        </div>
+			      	</li>
+
+			      	<li class="nav-item dropdown">
+			        	<a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+			          		Directories
+			        	</a>
+				        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+				        	<a class="dropdown-item" href="/cpsc304/user-directory.php">Users</a>
+				        	<a class="dropdown-item" href="/cpsc304/hotline-directory.php">Hotlines</a>
+				        	<a class="dropdown-item" href="/cpsc304/resource-centre-directory.php">Resource Centers</a>
+				        	<a class="dropdown-item" href="/cpsc304/types-of-help-directory.php">Types of Help</a>
+				        </div>
+			      	</li>
+			      	
+			    </ul>
+	  		</div>
+        </nav>
+		<!-- Page content -->
 		<div class = "container">
-			<h1 class = "text-center mt-5 mb-5"> Review Has Posted Confirmation </h1>
-            <?php displayContents()?>
-            <div> Return to Write Review page <a href="./write-reviews.php"> here</a></div>
-        </div>
-    </body> 
+        
+			<h1 class = "text-center mt-5 mb-5"> Update Reviews </h1>
+			<form action="./write-reviews.php" method="post" >
+                <div class="form-group">
+                    <select class="form-control" name="userID" id="userID"> 
+                    <?php 
+                    	if($_SESSION["userType"] == "helpSeeker") {
+							showReviewForm();
+						}
+			 		?>
+                    </select>
+                </div>
+
+                <div class="form-group">
+                    <label for="reviewAuthor" id="reviewAuthor">Your userID: </label>
+                    <input name="reviewAuthor" id="reviewAuthor" required></input>
+                </div>
+
+                <div class="form-group col-md-5">
+                    <label for="counsellor" id="counsellor">Your counsellor's ID: </label>
+                    <input type="number" name="counsellor" required></input>
+                </div>
+
+                <div class="form-group col-md-3">
+                    <label for="rating" id="rating">Please rate: </label>
+                    <input type="number" name="rating" required></input>
+                </div>
+
+                <div class="form-group col-md-3">
+                    <label for="feedback" id="feedback">Please give us your feedback: </label>
+                    <input type="text" name="feedback" required></input>
+                </div>
+
+                <button class="btn btn-success" type="submit" value="submit">Sign up </button>
+            </form>
+
+		</div>
+	</body>
 </html>
